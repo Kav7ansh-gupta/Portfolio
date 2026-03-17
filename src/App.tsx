8f8c310434022a2082fc2bd8247d30c33f5f703c
kav7ansh-gupta <kavyanshgupta119@gmail.com>
2026-03-17 15:31:16 +0530

    Hero Section Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import SparkleNavbar from "./Components/Navbar";
 import Skills from "./Pages/Skils";
 import About from "./Pages/About";
 import Certifications from "./Pages/Certifications";
-import Dock from "./Components/Dock";
-import { FcHome } from "react-icons/fc";
-import { GrCertificate } from "react-icons/gr";
-import { SiAboutdotme, SiSkillshare } from "react-icons/si";
-import { GoProjectSymlink } from "react-icons/go";
 import Footer from "./Components/Footer";
 import { Routes, Route } from "react-router-dom";
 import Projects from "./Pages/Projects";
@@ -21,14 +16,6 @@ const App = () => {
     { name: "Cerification", path: "/certification" },
   ];
 
-  const dockItems = [
-    { icon: <FcHome />, label: "Home", to: "/" },
-    { icon: <SiAboutdotme />, label: "About", to: "/about" },
-    { icon: <SiSkillshare />, label: "Skills", to: "/skills" },
-    { icon: <GoProjectSymlink />, label: "Project", to: "/projects" },
-    { icon: <GrCertificate />, label: "Certification", to: "/certification" },
-  ];
-
   return (
     <>
       <div className="">
@@ -40,12 +27,6 @@ const App = () => {
           <Route path="/projects" element={<Projects />} />
           <Route path="/certification" element={<Certifications />} />
         </Routes>
-        {/* <Dock
-          items={dockItems}
-          position="bottom"
-          panelHeight={75}
-          baseItemSize={50}
-        /> */}
         <Footer />
       </div>
     </>
